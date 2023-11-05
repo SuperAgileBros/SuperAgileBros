9e5f80e7f46dde23180a36a862adc35cd50466b7
Michał Pańczyk <mpanczyk@protnmail.com>
2023-11-05 15:17:41 +0100

    moving left and right

--- actors/src/Actor.gd
+++ actors/src/Actor.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 class_name Actor
 
-export var speed: = Vector2(300.0,1000.0)
+export var speed: = Vector2(800.0,1000.0)
 export var gravity: = 3000.0
 
 var velocity: = Vector2.ZERO
@@ -10,4 +10,3 @@ func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	velocity.y = min(velocity.y, speed.y)
 	velocity.x = min(velocity.x, speed.x)
-	velocity = move_and_slide(velocity)
--- actors/src/Player.gd
+++ actors/src/Player.gd
@@ -1,1 +1,9 @@
 extends Actor
+
+func _physics_process(delta: float) -> void:
+    var direction: = Vector2(
+        Input.get_action_strength("character_right") - Input.get_action_strength("character_left"),
+        1.0
+    )
+    velocity = direction * speed
+    velocity = move_and_slide(velocity)
