0cc17c2b965a05819955294df3fca99798f2b653
Michał Pańczyk <mpanczyk@protnmail.com>
2023-11-08 21:39:38 +0100

    tabbed view of Menu_key_binding.tscn for each connected controler

--- menus/src/Menu_key_binding.gd
+++ menus/src/Menu_key_binding.gd
@@ -1,26 +1,18 @@
 extends Control
 
-var can_change_key = false
+var can_change_key = true
 var action_string
 var actions = ""
 
+var ControlerTab = preload("res://menus/ui/Controler_tab.tscn")
+
 func _ready():
 	actions = InputMap.get_actions()
-	_set_keys()
-	
-
-func _set_keys():
-	for i in actions:
-		if i.begins_with("character"):
-			_bindingNode(i,InputMap.get_action_list(i)[0].as_text(),"Panel/ScrollContainer/VBoxContainer/VBoxCharacterBindings")
-
-func _bindingNode(action,binding,parent):
-	var container = HBoxContainer.new()
-	
-	container.add_child(Label.new())
-	container.add_child(Button.new())
-	
-	container.get_child(0).text = action.split("_")[1]
-	container.get_child(1).text = binding
-	
-	get_node(parent).add_child(container)
+	var joypads = Input.get_connected_joypads()
+	var newTab = ControlerTab.instance()
+	newTab.name = "Keyboard"
+	add_child(newTab)
+	for joypad in joypads:
+		newTab = ControlerTab.instance()
+		newTab.name = Input.get_joy_name(joypad) + " " + str(joypad)
+		add_child(newTab)
