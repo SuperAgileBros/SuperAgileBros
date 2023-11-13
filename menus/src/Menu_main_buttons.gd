b7bafe07ccc80ffcb9eb98ea0947bd5949312f24
Michał Pańczyk <mpanczyk@protnmail.com>
2023-11-13 21:21:45 +0100

    basic main menu and updated gitignore

---
+++ menus/src/Menu_main_buttons.gd
@@ -0,0 +1,20 @@
+extends VBoxContainer
+
+var tutorial = "res://lvls/Level_tutorial.tscn"
+var new_game = "res://lvls/Level_tutorial.tscn"
+var load_game = "res://lvls/Level_tutorial.tscn"
+var settings = "res://menus/Menu_key_binding.tscn"
+
+func _ready():
+	get_node("TutorialButton").connect("pressed", self, "_on_button_pressed", [tutorial])
+	get_node("NewGameButton").connect("pressed", self, "_on_button_pressed", [new_game])
+	get_node("LoadGameButton").connect("pressed", self, "_on_button_pressed", [load_game])
+	get_node("SettingsButton").connect("pressed", self, "_on_button_pressed", [settings])
+	get_node("ExitButton").connect("pressed", self, "_on_quit_pressed")
+
+
+func _on_button_pressed(path):
+	get_tree().change_scene(path)
+
+func _on_quit_pressed():
+	get_tree().quit()
