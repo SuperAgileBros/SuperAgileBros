ef04f81902b519e1b49504e123e64b6f9b19267e
TomaszSienko <tomek252925@gmail.com>
2023-11-16 19:55:10 +0100

    Modified character choose function, now it switches Kinematic Bodies on scene not only the texture

--- lvls/src/Character_choose.gd
+++ lvls/src/Character_choose.gd
@@ -10,22 +10,32 @@ extends Panel
 func _ready():
 	pass # Replace with function body.
 
-var tomek = preload("res://actors/assets/tomek.png")
-var mati = preload("res://actors/assets/mati.png")
-var michal = preload("res://actors/assets/michal.png")
-var kamil = preload("res://actors/assets/kamil.png")
-
-onready var dummy_object = get_node("/root/Node2D/CollisionPolygon2D/CollisionPolygon2D/Sprite")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+var tomekCharacter : PackedScene = preload("res://actors/Tomek.tscn")
+var matCharacter : PackedScene = preload("res://actors/Mat.tscn")
+var kamilCharacter : PackedScene = preload("res://actors/Kamil.tscn")
+var michalCharacter : PackedScene = preload("res://actors/Michal.tscn")
+onready var currentKinematicBody = get_node("/root/Node2D/CollisionPolygon2D")
 
 func _process(delta): 
-	if  self.visible:
-		if Input.is_action_just_pressed("character_right"):
-			dummy_object.set_texture(michal)
-		if Input.is_action_just_pressed("character_left"):
-			dummy_object.set_texture(kamil)
+	if  self.visible:	
 		if Input.is_action_just_pressed("character_jump"):
-			dummy_object.set_texture(mati)
+			_characterSwitch(matCharacter)
+			
 		if Input.is_action_just_pressed("character_down"):
-			dummy_object.set_texture(tomek)
-
+			_characterSwitch(tomekCharacter)
+		
+		if Input.is_action_just_pressed("character_right"):
+			_characterSwitch(michalCharacter)
+		
+		if Input.is_action_just_pressed("character_left"):
+			_characterSwitch(kamilCharacter)
+			
+func _characterSwitch(choosenCharacter):
+	var previousKinematicBody = currentKinematicBody
+	var currentindex = currentKinematicBody.get_index()
+	currentKinematicBody.queue_free()
+	currentKinematicBody = choosenCharacter.instance()
+	currentKinematicBody.position = previousKinematicBody.position
+	currentKinematicBody.z_index = -1
+	get_parent().add_child(currentKinematicBody,currentindex)
