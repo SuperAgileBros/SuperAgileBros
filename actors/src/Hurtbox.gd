567980bf291260b48428f913cbf968b09bbbaba0
Michał Pańczyk <mpanczyk@protnmail.com>
2023-12-22 21:55:40 +0100

    spawner and better slime damage

--- actors/src/Hurtbox.gd
+++ actors/src/Hurtbox.gd
@@ -10,11 +10,15 @@ func _on_area_entered(hitbox):
 		print("hitbox is null")
 		return
 	if hitbox is Hitbox and owner.has_method("take_damage"):
-		
 		print(owner.name + " damaged for " + str(hitbox.damage) + " by " + str(hitbox.get_parent().get_parent().name))
 		owner.take_damage(hitbox.damage)
+		if owner.has_method("knockback"):
+			owner.knockback(1000, hitbox.get_parent().get_parent().global_position)
+
 	if hitbox.owner != null:
 		if hitbox.owner.has_method("work"):
 			hitbox.owner.work()
+		if hitbox.owner.has_method("move_and_slide"):
+			hitbox.owner.move_and_slide((hitbox.global_position - self.global_position)*100)
 	if hitbox is Player:
 		hitbox.take_damage(owner.attack)
--- actors/src/Slime1.gd
+++ actors/src/Slime1.gd
@@ -1,16 +1,16 @@
-extends KinematicBody2D
+extends Enemy
 class_name Slime
 
 var velocity = Vector2.ZERO
 var gravity = 800
-var jump_power = 250
+var max_speed = 200
+var jump_power = 200
 var jump_timer = 0
 var jump_interval_min = 1.0
 var jump_interval_max = 2.0
 var move_timer = 0
 var move_interval_min = 1.0
 var move_interval_max = 2.0
-onready var health = 40
 var damage = 10
 var distance
 var proximity_range = 300
@@ -23,12 +23,13 @@ func _ready():
 	# Pobieram instancje gracza za pomocą nazwy, udało mi sie w skrypcie do zmiany gracza
 	# przypisać wartość "Name" taką jaką miał poprzedni obiekt
 	player = get_parent().get_node("Player")
-	$CollisionPolygon2D/Sprite/Hitbox.damage = damage
+	$Hitbox.damage = damage
 	#connect("body_entered", self, "_on_RigidBody2D_body_entered")
 
 
 func _process(delta):
 	velocity.y += gravity * delta
+	velocity.y = clamp(velocity.y, -max_speed, max_speed)
 	
 	if player and is_instance_valid(player):
 		# Oblicz gdzie jest gracz
@@ -37,7 +38,7 @@ func _process(delta):
 			var direction_to_player = (player.global_position - global_position).normalized()
 
 		# Ustawia prędkosć względem gracza
-			velocity.x = direction_to_player.x * 350  # Tu możemy zwiększyć prędkosć slime
+			velocity.x = direction_to_player.x * 150  # Tu możemy zwiększyć prędkosć slime
 	else:
 		# Jak funkcja zmiany postaci usuwa gracza na sam moment wyboru to slime musi sobie poczekać
 		# chwilowo
@@ -47,9 +48,6 @@ func _process(delta):
 			move_timer = rand_range(move_interval_min, move_interval_max)
 			move_direction *= -1  # Randomowo zmioenia kierunek
 		velocity.x = move_direction * 100  # random prędkość
-	# Funkcja poruszania
-	#velocity = move_and_slide(velocity, Vector2.UP)
-	velocity = move_and_slide(velocity, Vector2.UP, false, 4, 0.785398, false)
 	#collision_process()	
 	# timer random skoku
 	jump_timer -= delta
@@ -58,6 +56,10 @@ func _process(delta):
 	if jump_timer <= 0:
 		jump_timer = rand_range(jump_interval_min, jump_interval_max)
 		jump()
+	# Funkcja poruszania
+	#velocity = move_and_slide(velocity, Vector2.UP)
+	velocity = move_and_slide(velocity, Vector2.UP, false, 4, 0.785398, false)
+
 	if health <= 0:
 		animation.play("Death",false)
 
@@ -66,25 +68,11 @@ func jump():
 	if is_on_floor():
 		# predkosc skoku
 		velocity.y = -jump_power
-		
-func take_damage(damage):
-	var dmg_label = load("res://EnvInteractions/FadingTekst.tscn").instance()
-	dmg_label.text = str(damage)
-	add_child(dmg_label)
-	dmg_label.get_child(0).play("Fade")
-	$Hit.play()
-	health = health - damage
+
+func knockback(force, source_position):
+	var direction = (global_position - source_position).normalized()
+	velocity += direction * force
 
 func die():
 	queue_free()
-	
-#func collision_process():
-#	for i in range(get_slide_count()):
-#		var collision = get_slide_collision(i)
-#		
-#		if collision.collider is KinematicBody2D:
-#			player.health = player.health - 1
-#			get_parent().set_health_bar()
-#		if collision.collider is Item:
-#		   health = health - collision.collider.damage
 
