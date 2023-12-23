53ab4b3dd57eae804a3b97f9503f0e526d008e86
Michał Pańczyk <mpanczyk@protnmail.com>
2023-12-23 21:32:21 +0100

    theme and resolution settings

--- menus/src/Menu_settings.gd
+++ menus/src/Menu_settings.gd
@@ -2,6 +2,8 @@ extends Control
 
 func _ready():
 	rect_size = get_parent_area_size()
+	if get_parent() == Popup:
+		$Background.visible = false
 	
 func _on_Back_pressed():
 	get_tree().change_scene("res://menus/Menu_main.tscn")
